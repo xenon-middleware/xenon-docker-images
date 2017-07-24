524749fa248d2a8b5ae4d5c102a64d3b13fec412
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2017-07-24 17:05:53 +0200

    using docker tags as per stefan's request

--- slurm/build-images.sh
+++ slurm/build-images.sh
@@ -19,18 +19,18 @@ docker build --tag supervisor .
 cd ..
 
 cd slurm-14
-docker build --tag slurm-14 .
+docker build --tag slurm:14 .
 cd ..
 
 cd slurm-15
-docker build --tag slurm-15 .
+docker build --tag slurm:15 .
 cd ..
 
 cd slurm-16
-docker build --tag slurm-16 .
+docker build --tag slurm:16 .
 cd ..
 
 cd slurm-17
-docker build --tag slurm-17 .
+docker build --tag slurm:17 .
 cd ..
 
