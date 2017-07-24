800c4daa2a83371c3f5a880673fd450db9fd1fa1
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2017-07-24 17:25:27 +0200

    adjusted to new names and added building of xenon-mysql

--- slurm/build-images.sh
+++ slurm/build-images.sh
@@ -1,36 +1,40 @@
-cd fixture
-docker build --tag fixture .
+cd xenon-fixture
+docker build --tag xenon-fixture .
 cd ..
 
-cd ssh
-docker build --tag ssh .
+cd xenon-ssh
+docker build --tag xenon-ssh .
 cd ..
 
-cd munge
-docker build --tag munge .
+cd xenon-munge
+docker build --tag xenon-munge .
 cd ..
 
-cd slurm-abstract
-docker build --tag slurm-abstract .
+cd xenon-mysql
+docker build --tag xenon-mysql .
 cd ..
 
-cd supervisor
-docker build --tag supervisor .
+cd xenon-slurm-abstract
+docker build --tag xenon-slurm-abstract .
 cd ..
 
-cd slurm-14
-docker build --tag slurm:14 .
+cd xenon-supervisor
+docker build --tag xenon-supervisor .
 cd ..
 
-cd slurm-15
-docker build --tag slurm:15 .
+cd xenon-slurm-14
+docker build --tag xenon-slurm:14 .
 cd ..
 
-cd slurm-16
-docker build --tag slurm:16 .
+cd xenon-slurm-15
+docker build --tag xenon-slurm:15 .
 cd ..
 
-cd slurm-17
-docker build --tag slurm:17 .
+cd xenon-slurm-16
+docker build --tag xenon-slurm:16 .
+cd ..
+
+cd xenon-slurm-17
+docker build --tag xenon-slurm:17 .
 cd ..
 
