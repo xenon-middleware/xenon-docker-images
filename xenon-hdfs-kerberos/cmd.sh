401d634d5ec34e311d60813380b67168bcdc86bf
Atze van der Ploeg <a.vanderploeg@escience.nl>
2017-10-30 17:15:19 +0100

    Fixes hdfs tests

--- xenon-hdfs-kerberos/cmd.sh
+++ xenon-hdfs-kerberos/cmd.sh
@@ -5,6 +5,8 @@ sleep 5s
 hdfs datanode &
 sudo -u xenon kinit -k -t /home/xenon/xenon.keytab xenon@esciencecenter.nl
 sudo -u xenon hdfs dfsadmin -safemode wait 
-touch /opt/hadoop/up 
+sleep 5s
+sudo -u xenon hdfs dfsadmin -safemode wait 
 echo DONE!!!!!!!!!!!
+touch /opt/hadoop/up 
 wait
