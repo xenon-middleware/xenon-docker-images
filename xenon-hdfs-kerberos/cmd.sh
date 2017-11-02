c00cb355be63ea94a245cc084e03cc17f338a3b9
Atze van der Ploeg <a.vanderploeg@escience.nl>
2017-11-02 15:02:32 +0100

    Finally done with HDFS!

--- xenon-hdfs-kerberos/cmd.sh
+++ xenon-hdfs-kerberos/cmd.sh
@@ -1,11 +1,11 @@
 /usr/bin/supervisord -c /etc/supervisord.conf &
-sleep 5s
+sleep 3s
 hdfs namenode & 
-sleep 5s 
+sleep 3s 
 hdfs datanode &
 sudo -u xenon kinit -k -t /home/xenon/xenon.keytab xenon@esciencecenter.nl
 sudo -u xenon hdfs dfsadmin -safemode wait 
-sleep 5s
+sleep 3s
 sudo -u xenon hdfs dfsadmin -safemode wait 
 echo DONE!!!!!!!!!!!
 touch /opt/hadoop/up 
--- xenon-hdfs/cmd.sh
+++ xenon-hdfs/cmd.sh
@@ -1,7 +1,8 @@
 hdfs namenode & 
-sleep 5s 
+sleep 3s 
 hdfs datanode &
 sudo -u xenon hdfs dfsadmin -safemode wait 
+sleep 3s
 touch /opt/hadoop/up 
 echo DONE!!!!!!!!!!!
 wait
