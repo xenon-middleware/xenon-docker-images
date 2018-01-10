f80d9a967d3a52843268e04d90b66d5d121a633d
Jason Maassen <jmaassen73@gmail.com>
2018-01-10 17:16:22 +0100

    Fixed environment passing in HDFS setup

--- xenon-hdfs-kerberos/cmd.sh
+++ xenon-hdfs-kerberos/cmd.sh
@@ -8,14 +8,14 @@ sleep 5s
 sudo -u xenon kinit -k -t /home/xenon/xenon.keytab xenon@esciencecenter.nl
 
 chmod a+x /loginxenon.sh
-sudo -u xenon /loginxenon.sh
-sudo -u xenon hdfs dfsadmin -safemode wait
-sudo -u xenon ./bin/hdfs dfs -mkdir /filesystem-test-fixture
-sudo -u xenon ./bin/hdfs dfs -mkdir /filesystem-test-fixture/links
-sudo -u xenon echo "Hello World" > file0
-sudo -u xenon ./bin/hdfs dfs -put file0 /filesystem-test-fixture/links/
-sudo -u xenon echo "" > file1
-sudo -u xenon ./bin/hdfs dfs -put file1 /filesystem-test-fixture/links/
+sudo -E -u xenon /loginxenon.sh
+sudo -E -u xenon hdfs dfsadmin -safemode wait
+sudo -E -u xenon ./bin/hdfs dfs -mkdir /filesystem-test-fixture
+sudo -E -u xenon ./bin/hdfs dfs -mkdir /filesystem-test-fixture/links
+sudo -E -u xenon echo "Hello World" > file0
+sudo -E -u xenon ./bin/hdfs dfs -put file0 /filesystem-test-fixture/links/
+sudo -E -u xenon echo "" > file1
+sudo -E -u xenon ./bin/hdfs dfs -put file1 /filesystem-test-fixture/links/
 
 echo DONE!!!!!!!!!!!
 touch /opt/hadoop/up
--- xenon-hdfs/cmd.sh
+++ xenon-hdfs/cmd.sh
@@ -1,15 +1,15 @@
-hadoop namenode -format
+hadoop namenode -format -force
 hdfs namenode &
 sleep 5s
 hdfs datanode &
 sleep 5s
-sudo -u xenon hdfs dfsadmin -safemode wait
-sudo -u xenon ./bin/hdfs dfs -mkdir /filesystem-test-fixture
-sudo -u xenon ./bin/hdfs dfs -mkdir /filesystem-test-fixture/links
-sudo -u xenon echo "Hello World" > file0
-sudo -u xenon ./bin/hdfs dfs -put file0 /filesystem-test-fixture/links/
-sudo -u xenon echo "" > file1
-sudo -u xenon ./bin/hdfs dfs -put file1 /filesystem-test-fixture/links/
+sudo -E -u xenon hdfs dfsadmin -safemode wait
+sudo -E -u xenon ./bin/hdfs dfs -mkdir /filesystem-test-fixture
+sudo -E -u xenon ./bin/hdfs dfs -mkdir /filesystem-test-fixture/links
+sudo -E -u xenon echo "Hello World" > file0
+sudo -E -u xenon ./bin/hdfs dfs -put file0 /filesystem-test-fixture/links/
+sudo -E -u xenon echo "" > file1
+sudo -E -u xenon ./bin/hdfs dfs -put file1 /filesystem-test-fixture/links/
 touch /opt/hadoop/up
-echo DONE!!!!!!!!!!!
+echo DONE!!!!!!!!!!
 wait
