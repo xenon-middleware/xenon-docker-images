c22c818f8f5e6fb1efd1ec3ebaf09407662a139f
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2018-10-26 14:44:32 +0200

    Fill hdfs during build instead of start up

--- xenon-hdfs-3/fill.sh
+++ xenon-hdfs-3/fill.sh
@@ -1,6 +1,16 @@
+#!/bin/sh
+
+hadoop namenode -format -force
+
+hdfs namenode &
+hdfs datanode &
+
 hdfs dfsadmin -safemode wait
 hdfs dfs -mkdir /filesystem-test-fixture
 hdfs dfs -mkdir /filesystem-test-fixture/links
 echo "Hello World" | hdfs dfs -put - /filesystem-test-fixture/links/file0
 echo "" | hdfs dfs -put - /filesystem-test-fixture/links/file1
 hdfs dfs -chown -R xenon:xenon /filesystem-test-fixture
+
+hdfs --daemon stop datanode
+hdfs --daemon stop namenode
