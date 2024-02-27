b1d077b0c7ae25679f1081ebc372883b99881d33
Stefan Verhoeven <stefan.verhoeven@gmail.com>
2024-02-27 11:44:15 +0100

    Create Docker image for slurm 23

---
+++ slurm-23/README.md
@@ -0,0 +1,7 @@
+# Slurm 23
+
+## Build
+
+```bash
+docker build -t ghcr.io/xenon-middleware/slurm:23 .
+```
---
+++ slurm-23/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo -e "\nstarting services..."
+systemctl start sshd munge slurmctld slurmrestd slurmdbd
+
+# TODO run slurmd instances with systemd
+echo -e "\nstarting compute nodes..."
+/usr/sbin/slurmd -D -N node-0 > /var/log/slurmd-node-0.out.log 2> /var/log/slurmd-node-0.err.log &
+/usr/sbin/slurmd -D -N node-1 > /var/log/slurmd-node-1.out.log 2> /var/log/slurmd-node-1.err.log &
+/usr/sbin/slurmd -D -N node-2 > /var/log/slurmd-node-2.out.log 2> /var/log/slurmd-node-2.err.log &
+/usr/sbin/slurmd -D -N node-3 > /var/log/slurmd-node-3.out.log 2> /var/log/slurmd-node-3.err.log &
+/usr/sbin/slurmd -D -N node-4 > /var/log/slurmd-node-4.out.log 2> /var/log/slurmd-node-4.err.log &
+
+sleep infinity
